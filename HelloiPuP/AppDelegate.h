7c4fb6977937986a41ffa24f14f3bb06cb5e2f8f
ipodishima <marian.paul2@gmail.com>
2012-02-15 15:28:06 +0100

    Initial Commit

---
+++ HelloiPuP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloiPuP
+//
+//  Created by Marian PAUL on 15/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
